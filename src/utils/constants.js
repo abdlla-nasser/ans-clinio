b9ee03e4b582287e97f11ac3473d2554e5c4b6e1
AhmedKamel95 <ahmedkamel095@gmail.com>
2020-07-12 14:05:11 +0200

    Changed URL to live

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,4 @@ export const GOOGLE_API_KEY = "AIzaSyBCroPaCtGmyUdbTP7EgSx09j_T0SxntaI";
 // const BASE_IP =
 //   process.env.NODE_ENV === "development" ? "5.189.142.97" : "192.168.15.15";
 
-// export const BASE_URL = `http://${BASE_IP}:9090/ords/`;
-export const BASE_URL = `http://localhost:8080/`;
+export const BASE_URL = `http://clinio-api-stg.tapps.studio/`;
